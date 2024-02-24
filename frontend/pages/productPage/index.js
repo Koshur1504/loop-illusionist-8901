d819ca9ea10dfc772c3fdc31aaecfa0c69606dd0
Koshur1504 <aamir.mir530@gmail.com>
2024-02-24 20:05:31 +0530

    Added functionality to products page

--- frontend/pages/productPage/index.js
+++ frontend/pages/productPage/index.js
@@ -1,154 +1,194 @@
-// product data 
-let details = document.querySelectorAll(".details")
-let three_details = document.querySelectorAll(".three-details")
-// let product_image=document.querySelector(".product-image")
+let details = document.querySelectorAll(".details");
+let three_details = document.querySelectorAll(".three-details");
+let products = [];
 
-let products = [
+let depURL = "http://127.0.0.1:5500/frontend/pages/productPage/index.html";
 
-    { id: 1, image: "https://is4.fwrdassets.com/images/p/ip2/pl2/TERX-WY497_V1.jpg", name: 'THE ROW', title: 'The Row Joe Backpack in Espresso', price: '165,411.98' },
-    { id: 2, image: "https://is4.fwrdassets.com/images/p/ip2/pl2/TERX-WY485_V1.jpg", name: 'THE ROW', title: 'The Row Joe Backpack in Espresso', price: '165,411.98' },
-    { id: 3, image: "https://is4.fwrdassets.com/images/p/ip2/pl2/TERX-WY490_V1.jpg", name: 'THE ROW', title: 'The Row Joe Backpack in Espresso', price: '165,411.98' },
-    { id: 4, image: "https://is4.fwrdassets.com/images/p/ip2/pl2/TERX-WY492_V1.jpg", name: 'THE ROW', title: 'The Row Joe Backpack in Espresso', price: '165,411.98' },
-    { id: 5, image: "https://is4.fwrdassets.com/images/p/ip2/pl2/TERX-WY462_V1.jpg", name: 'THE ROW', title: 'The Row Joe Backpack in Espresso', price: '165,411.98' },
-];
-
+let params = new URL(document.location).searchParams;
+let paramID = params.get("id");
 function rainderItems() {
-    product_image.setAttribute("src", products.image)
-    three_details.forEach(item => {
-
-        item.addEventListener("click", (e) => {
-
-            handelActive(e);
-        })
-    })
-    details[0].style.display = "block"
-    three_details[0].classList.add("visible")
+  product_image.setAttribute("src", products.image);
+  three_details.forEach((item) => {
+    item.addEventListener("click", (e) => {
+      handelActive(e);
+    });
+  });
+  details[0].style.display = "block";
+  three_details[0].classList.add("visible");
+  console.log(car1);
 }
 function handelActive(e) {
-
-    three_details.forEach(item => {
-        item.classList.remove("visible")
-    })
-    e.target.classList.add("visible")
+  three_details.forEach((item) => {
+    item.classList.remove("visible");
+  });
+  e.target.classList.add("visible");
 }
-// let productID = JSON.parse(localStorage.getItem("product")) || 1;
-// let baseurl = `https://loop-illusionist-8901.onrender.com/products/${productID}`;
-// async function getdata() {
-//   try {
-//     let data = await fetch(`${baseurl}`);
-//     products = await data.json();
-//     rainderItems();
-//     console.log(products);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-
-// getdata();
-
-// // Function to display products on the page
-// function displayProducts() {
-//     const productContainer = document.getElementById('product-container');
-//     // Clear existing content in the container
-//     productContainer.innerHTML = '';
-
-//     // Loop through each product and create a product card
-//     products.forEach(product => {
-//         const productCard = document.createElement('div');
-//         productCard.className = 'product-card';
-//         productCard.style.border = "none";
-//         productCard.style.marginLeft = "0px";
-//         productCard.innerHTML = `
-//         <img src="${product.image}"/>
-//             <p>${product.name}</p>
-//             <p>${product.title}</p>
-//             <p>${product.price}</p>
-//         `;
-
-//         // Add the product card to the container
-//         productContainer.appendChild(productCard);
-//     });
-
-// }
-
-// displayProducts();
-
-////viewed
-
-
-
-// function displayProductsView() {
-//     const productContainerviewed = document.getElementById('product-viewed');
-//     // Clear existing content in the container
-
-//     productContainerviewed.innerHTML = '';
-
-//     // Loop through each product and create a product card
-//     products.forEach(product => {
-//         const productCard = document.createElement('div');
-//         productCard.className = 'product-card';
-//         // productCard.style.border = "none";
-//         // productCard.style.marginLeft = "0px";
-
-//         productCard.innerHTML = `
-//         <img src="${product.image}"/>
-//             <p>${product.name}</p>
-//             <p>${product.title}</p>
-//             <p>${product.price}</p>
-//         `;
-
-//         // Add the product card to the container
-
-//         productContainerviewed.appendChild(productCard);
-//     });
-// }
-// displayProductsView();
-
-//
 
 var expanded = false;
 
 function toggleContent() {
-    var content = document.getElementById('content');
-    var viewToggleBtn = document.getElementById('viewToggleBtn');
+  var content = document.getElementById("content");
+  var viewToggleBtn = document.getElementById("viewToggleBtn");
 
-    if (!expanded) {
-        content.style.maxHeight = '100%';
-        viewToggleBtn.textContent = 'View Less';
-    } else {
-        content.style.maxHeight = '600px';
-        viewToggleBtn.textContent = 'View More';
-    }
-    expanded = !expanded;
-
+  if (!expanded) {
+    content.style.maxHeight = "100%";
+    viewToggleBtn.textContent = "View Less";
+  } else {
+    content.style.maxHeight = "600px";
+    viewToggleBtn.textContent = "View More";
+  }
+  expanded = !expanded;
 }
 viewToggleBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-})
-
+  e.preventDefault();
+});
 
 /////
 
-var links = document.querySelectorAll('a');
+var links = document.querySelectorAll("a");
 
-links.forEach(function (link) {
-    link.addEventListener('click', function (event) {
-        event.preventDefault(); // Prevent the default link behavior
-        var detailsId = link.getAttribute('data-details');
-        showDetails(detailsId);
-    });
-});
+// links.forEach(function (link) {
+//   link.addEventListener("click", function (event) {
+//     event.preventDefault(); // Prevent the default link behavior
+//     var detailsId = link.getAttribute("data-details");
+//     showDetails(detailsId);
+//   });
+// });
 
 function showDetails(detailsId) {
-    // Hide all details divs
-    var allDetails = document.querySelectorAll('.details');
-    allDetails.forEach(function (details) {
-        details.style.display = 'none';
-    });
+  // Hide all details divs
+  var allDetails = document.querySelectorAll(".details");
+  allDetails.forEach(function (details) {
+    details.style.display = "none";
+  });
 
-    // Show the selected details div
-    var selectedDetails = document.getElementById(detailsId);
-    selectedDetails.style.display = 'block';
+  // Show the selected details div
+  var selectedDetails = document.getElementById(detailsId);
+  selectedDetails.style.display = "block";
 }
 
+let product = {
+  id: 1,
+  title: "favorite Strapless Bill Pickleball Dress",
+  original_src:
+    "https://www.fwrd.com/product-norma-kamali-strapless-bill-pickleball-dress-in-black-snow-white/NOMF-WD478/?d=Womens&itrownum=1&itcurrpage=1&itview=05",
+  image: "https://is4.fwrdassets.com/images/p/fw/p/NOMF-WD478_V1.jpg",
+  brand: "Norma Kamali",
+  description: "Strapless Bill Pickleball Dress",
+  price: 32833,
+  price_sale: null,
+  gender: "female",
+  category: "clothing",
+};
+let baseURL = "https://loop-illusionist-8901.onrender.com/products";
+let productID = paramID || 1;
+async function getProduct() {
+  try {
+    let url = `${baseURL}/${productID}`;
+    let data = await fetch(url);
+    product = await data.json();
+    setProductsDetails();
+  } catch (error) {
+    console.log(error);
+  }
+}
+getProduct();
 
+let car1 = document.querySelectorAll("#car1 > div > img");
+
+function setProductsDetails() {
+  car1.forEach((item) => {
+    item.src = product.image;
+  });
+  document.querySelector(".v-container2 .brand").innerHTML = product.brand;
+  document.querySelector(".v-container2 .title").innerHTML = product.title;
+  document.querySelector(".v-container2 .price").innerHTML = product.price;
+  document.querySelector(".v-container2 .price").innerHTML = product.price;
+}
+
+function creatCard(item) {
+  let card = document.createElement("div");
+  card.addEventListener("click", () => {
+    let id = item.id;
+    window.location.replace(`${depURL}?id=${id}`);
+  });
+  card.classList.add("card");
+  card.style.width = "16rem";
+  card.innerHTML = `<img
+  src=${item.image}
+  class="card-img-top"
+  alt=${item.description}
+/>
+<div class="card-body">
+  <h6 class="card-title">${item.brand}</h6>
+  <p class="card-text">${item.title}</p>
+  <p class="card-text">₹${item.price}</p>
+</div>`;
+  return card;
+}
+
+// mayLike
+let mayLike = [];
+
+async function getMaylike() {
+  let gender = product.gender;
+  let low = Math.ceil(Math.random() * 100);
+  let data = await fetch(`${baseURL}?gender=${gender}&_start=${low}&_limit=10`);
+  mayLike = await data.json();
+  appendmayLikeCards();
+}
+function appendmayLikeCards() {
+  let container = document.getElementById("content");
+  container.innerHTML = "";
+  mayLike.forEach((item) => {
+    container.append(creatCard(item));
+  });
+}
+getMaylike();
+
+// recently viewed
+let recently = [];
+
+async function getRecently() {
+  let gender = product.gender;
+  let low = Math.ceil(Math.random() * 100);
+  let data = await fetch(`${baseURL}?gender=${gender}&_start=${low}&_limit=10`);
+  recently = await data.json();
+  appendrecentLiked();
+}
+function appendrecentLiked() {
+  let container = document.getElementById("rcv");
+  container.innerHTML = "";
+  recently.forEach((item) => {
+    container.append(creatCard(item));
+  });
+}
+getRecently();
+
+// cart
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+document.querySelector("#cartButton").addEventListener("click", addToCart);
+function addToCart() {
+  cart.push(product);
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+// wishlist
+function hasItem(arr, id) {
+  for (const item of arr) {
+    if (item.id === id) {
+      return true;
+    }
+  }
+  return false;
+}
+let wishlist = JSON.parse(localStorage.getItem("wishlist")) || [];
+document.querySelector("#wishlist").addEventListener("click", addToWishlist);
+function addToWishlist() {
+  if (!hasItem(wishlist, product.id)) {
+    wishlist = [product, ...wishlist];
+    localStorage.setItem("wishlist", JSON.stringify(wishlist));
+  } else {
+    alert("item already in wishlist");
+  }
+}
