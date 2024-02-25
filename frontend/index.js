19b55fa5b5a952185ddc0a78ca776a268200ca66
Aamir mir <132262142+Koshur1504@users.noreply.github.com>
2024-02-25 14:10:13 +0530

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,7 @@
 const isDevelopment = window.location.hostname.includes("127.0.0.1");
 let baseURL = isDevelopment
   ? "http://127.0.0.1:5500/frontend"
-  : "https://loop-illusionist-8901-1.onrender.com/";
+  : "https://loop-illusionist-8901-1.onrender.com";
 
 let cat_links = document.querySelectorAll("#cat_links > ul > li > a");
 let cards = document.querySelectorAll(".productimg1 > divs");
